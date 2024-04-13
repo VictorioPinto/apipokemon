560b0c60e5bb2a3b4f5e57116d63ba0d1a46b901
VictorioPinto <162982794+VictorioPinto@users.noreply.github.com>
2024-04-12 23:46:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,61 @@
-const pokemon = document.getElementById("pokemon");
-const id = document.getElementById("input0");
-const sexo = document.getElementById("sexo");
-const shiny = document.getElementById("shiny");
-const lado = document.getElementById("lado");
-const pkmnome = document.getElementById("pkmnome");
-const fetchpokemon = async (id) => {
-  const request = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${id.toLowerCase()}`
-  );
-  const data = await request.json();
-  let imagem;
-  pokemon.style.display = "Block";
-  pkmnome.textContent = data.name;
-  if (id === "") {
-    alert("Escreva o numero do pokemon que deseja buscar!");
-  } else if (
-    sexo.value == "femea" &&
-    lado.value == "frente" &&
-    shiny.value == "normal"
-  ) {
-    imagem = data.sprites.front_female;
-    if (data.sprites.front_female == null) {
-      alert(`o pokemon ${data.name} nao possui uma forma femea`);
-      imagem = data.sprites.front_default;
-    }
-  } else if (
-    sexo.value == "femea" &&
-    lado.value == "costa" &&
-    shiny.value == "normal"
-  ) {
-    imagem = data.sprites.back_female;
-    if (data.sprites.back_female == null) {
-      alert(`o pokemon ${data.name} nao possui uma forma femea`);
-      imagem = data.sprites.back_default;
-    }
-  } else if (
-    sexo.value == "femea" &&
-    lado.value == "frente" &&
-    shiny.value == "shiny"
-  ) {
-    imagem = data.sprites.front_shiny_female;
-    if (data.sprites.front_shiny_female == null) {
-      alert(`o pokemon ${data.name} nao possui uma forma femea`);
-      imagem = data.sprites.front_shiny;
-    }
-  } else if (
-    sexo.value == "femea" &&
-    lado.value == "costa" &&
-    shiny.value == "shiny"
-  ) {
-    imagem = data.sprites.back_shiny_female;
-    if (data.sprites.back_shiny_female == null) {
-      alert(`o pokemon ${data.name} nao possui uma forma femea`);
-      imagem = data.sprites.back_shiny;
-    }
-  } else if (
-    sexo.value == "macho" &&
-    lado.value == "costa" &&
-    shiny.value == "normal"
-  ) {
-    imagem = data.sprites.back_default;
-  } else if (
-    sexo.value == "macho" &&
-    lado.value == "frente" &&
-    shiny.value == "normal"
-  ) {
-    imagem = data.sprites.front_default;
-  } else if (
-    sexo.value == "macho" &&
-    lado.value == "costa" &&
-    shiny.value == "shiny"
-  ) {
-    imagem = data.sprites.back_shiny;
-  } else if (
-    sexo.value == "macho" &&
-    lado.value == "frente" &&
-    shiny.value == "shiny"
-  ) {
-    imagem = data.sprites.front_shiny;
-  }
-  pokemon.src = imagem;
-};
-document.getElementsByTagName("button")[0].addEventListener("click", () => {
-  fetchpokemon(id.value);
-});
-document.addEventListener("DOMContentLoaded", function () {
-  pokemon.style.display = "none";
-});
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: black;
+  color: rgb(255, 102, 0);
+  font-family: "Fira Code", monospace;
+}
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#input0 {
+  height: 4vh;
+  font-size: 1rem;
+  width: 30vh;
+  margin: 5vh 0;
+  text-align: center;
+}
+#input0:focus {
+  border: 0;
+  outline: 0;
+}
+select {
+  width: 10vh;
+  height: 3vh;
+  margin: 0 2vh;
+}
+div img {
+  width: 15vh;
+  height: 15vh;
+}
+button {
+  width: 10vh;
+  height: 3vh;
+}
+#selects {
+  display: flex;
+  align-items: center;
+  padding-top: 5vh;
+}
+.btns {
+  background-color: rgb(255, 102, 0);
+  margin-top: 2vh;
+  height: 4vh;
+  width: 12vh;
+  font-size: 1em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0;
+  border-radius: 50px;
+}
+.btns:hover {
+  cursor: pointer;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
